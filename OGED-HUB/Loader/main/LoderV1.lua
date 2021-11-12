0cfdffd53035d91c039b7e992ed8cf2d3f77f236
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2021-11-12 01:13:21 -0500

    Update LoderV1.lua

--- OGED-HUB/Loader/main/LoderV1.lua
+++ OGED-HUB/Loader/main/LoderV1.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/JRL-lav/Main/main/V2"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xNiKlaSTHx/Script-main-Blox-Fruit/main/OGED-HUB/connectV1loder.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/xNiKlaSTHx/Script-main-Blox-Fruit/main/OGED-HUB/connectV1loder.lua"))()
