365dccad5281e541b95bef7cacc9eb42c9ec0bc2
xNiKlaSTHx <88712393+xNiKlaSTHx@users.noreply.github.com>
2021-12-06 20:05:23 -0500

    Update LoderV1.lua

--- OGED-HUB/Loader/main/LoderV1.lua
+++ OGED-HUB/Loader/main/LoderV1.lua
@@ -1,2 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/JRL-lav/Main/main/V2"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/xNiKlaSTHx/Script-main-Blox-Fruit/main/OGED-HUB/connectV1loder.lua"))()
+game.Players.LocalPlayer:Kick("เลิก Support ละสัส อ่านประกาศมั้งไอควายยย!")
+
+---loadstring(game:HttpGet("https://raw.githubusercontent.com/JRL-lav/Main/main/V2"))()
+---loadstring(game:HttpGet("https://raw.githubusercontent.com/xNiKlaSTHx/Script-main-Blox-Fruit/main/OGED-HUB/connectV1loder.lua"))()
